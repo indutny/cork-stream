cef911c8b25f4ff20e96935d83d3e978b11bb314
Fedor Indutny <fedor@indutny.com>
2017-06-05 21:32:24 -0400

    initial

---
+++ test/cork-test.js
@@ -0,0 +1,102 @@
+'use strict';
+/* global describe it */
+
+const assert = require('assert');
+const PassThrough = require('stream').PassThrough;
+
+const CorkStream = require('../');
+
+describe('cork-stream', () => {
+  it('should propagate reads', (cb) => {
+    const fake = new PassThrough();
+
+    const cork = new CorkStream(fake);
+
+    let chunks = '';
+    cork.on('data', chunk => chunks += chunk);
+    cork.once('end', () => {
+      assert.equal(chunks, 'hello world!');
+      cb();
+    });
+
+    fake.write('hello');
+    fake.write(' ');
+    setImmediate(() => {
+      fake.write('world');
+      fake.end('!');
+    });
+  });
+
+  it('should propagate writes', (cb) => {
+    const fake = new PassThrough();
+
+    const cork = new CorkStream(fake);
+
+    cork.write('hello');
+    cork.write(' ');
+    setImmediate(() => {
+      cork.write('world');
+      cork.end('!');
+    });
+
+    let chunks = '';
+    fake.on('data', chunk => chunks += chunk);
+    fake.once('end', () => {
+      assert.equal(chunks, 'hello world!');
+      cb();
+    });
+  });
+
+  it('should propagate errors', (cb) => {
+    const fake = new PassThrough();
+
+    const cork = new CorkStream(fake);
+
+    cork.once('error', (err) => {
+      assert(err);
+      cb();
+    });
+
+    fake.emit('error', new Error('ohai'));
+  });
+
+  it('should cork/uncork', (cb) => {
+    const log = [];
+    const fake = {
+      on: () => {},
+      once: () => {},
+
+      cork: () => log.push('cork'),
+      uncork: () => log.push('uncork'),
+      write: (data, enc, cb) => {
+        log.push(data.toString());
+        cb(null);
+      },
+      end: () => {
+        log.push('end');
+
+        assert.deepEqual(log, [
+          'cork',
+          'hello',
+          ' ',
+          'uncork',
+          'cork',
+          'world',
+          '!',
+          'uncork',
+          'end'
+        ]);
+        cb();
+      }
+    };
+
+    const cork = new CorkStream(fake);
+
+    cork.write('hello');
+    cork.write(' ');
+    setImmediate(() => {
+      cork.write('world');
+      cork.end('!');
+    });
+  });
+});
